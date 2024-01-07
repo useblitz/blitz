6d73ed4028f37dbbdb2e8e96a816c9c9bd3c0b6f
Aarvin R <144612896+aarvinr@users.noreply.github.com>
2024-01-07 09:04:47 -0800

    fix gitignore, onmount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,28 @@
   import "$lib/globals.css";
 
   import { pageTitle } from "$lib/globals";
+  import { db } from "$lib/api/database";
+
   import Header from "$lib/components/Header.svelte";
+
+  let pocketbase: PocketBase;
+
+  db.subscribe((v) => {
+    pocketbase = v;
+  });
+
+  onMount(() => {
+    fetch("/", { method: "GET" })
+      .then((res) => res.json())
+      .then((res) => {
+        pocketbase.authStore.save(res.token);
+      });
+  });
 </script>
 
 <svelte:head>
   <title>{$pageTitle} \ Blitz</title>
 </svelte:head>
 
-<Header />
+<header />
 <slot />
